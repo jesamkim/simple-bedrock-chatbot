9fd90ab3f13d0885eaa0b17db52eb14d181c835b
Jesam Kim <jesamkim@gmail.com>
2024-10-28 10:45:09 +0900

    Update config_file.py

--- CDK-deploy/docker_app/config_file.py
+++ CDK-deploy/docker_app/config_file.py
@@ -1,9 +1,9 @@
 class Config:
     # Stack name
     # Change this value if you want to create a new instance of the stack
-    STACK_NAME = "cdk-chatbot-claude3"
+    STACK_NAME = "cdk-chatbot-claude35"
     
     # Put your own custom value here to prevent ALB to accept requests from
     # other clients that CloudFront. You can choose any random string.
-    CUSTOM_HEADER_VALUE = "My_random_value_58dsv15e4s31"    
+    CUSTOM_HEADER_VALUE = "My_random_value_59asv15e4s32"    
     
